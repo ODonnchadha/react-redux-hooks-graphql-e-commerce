fd58952a6a5286cc78d099e8972710c3fa7e5c7c
ODonnchadha <danieldonahue1231@hotmail.com>
2020-02-03 05:56:50 -0600

    Initial header.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 
+import Header from './components/header/header';
 import Home from './pages/home/home';
 import Shop from './pages/shop/shop';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={Home} />
         <Route exact path='/shop' component={Shop} />
