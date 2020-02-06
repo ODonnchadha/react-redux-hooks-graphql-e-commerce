d7b8c9f3d6900f8afe5c3c632cd3a9da13b33b91
ODonnchadha <danieldonahue1231@hotmail.com>
2020-02-06 06:33:25 -0600

    Addition of onAuthStateChanged() method.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
-
+import { auth } from './firebase';
 import Header from './components/header/header';
-import Auth from './pages/auth/auth';
+import Authorization from './pages/authorization/authorization';
 import Home from './pages/home/home';
 import Shop from './pages/shop/shop';
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/shop' component={Shop} />
-        <Route exact path='/signin' component={Auth} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      currentUser: null
+    }
+  }
+  
+  unsubscribeFromAuth = null;
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+    });
+  }
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/shop' component={Shop} />
+          <Route exact path='/signin' component={Authorization} />
+        </Switch>
+      </div>
+    )
+  }
 }
 
 export default App;
